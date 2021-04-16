a047d8247bed81cc5e69e57e8680caeea6764913
RealPolitiX <xrpatrick@gmail.com>
2021-04-16 06:28:02 -0500

    Added the Octahedron class

--- crystmorph/__init__.py
+++ crystmorph/__init__.py
@@ -1,4 +1,13 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
+from . import polyhedron, transformation
+try:
+    from . import structure
+except:
+    pass
+
+
 __version__ = '0.0.0'
+__author__ = 'R. Patrick Xian'
--- crystmorph/polyhedron.py
+++ crystmorph/polyhedron.py
@@ -5,6 +5,7 @@
 @author: R. Patrick Xian
 """
 
+from . import transformation as trans
 import numpy as np
 
 
@@ -12,7 +13,7 @@ class ConvexPolyhedron(object):
     """ Root class for convex polyhedra.
     """
     
-    def __init__(self, center, n_vertex=None, n_edge=None):
+    def __init__(self, center=np.array([0, 0, 0]), n_vertex=None, n_edge=None):
         
         self.center = center
         self.n_vertex = n_vertex
@@ -62,7 +63,7 @@ def is_corner_sharing(ph1, ph2, **kwargs):
 
 
 class Face3D(object):
-    """ Polygon data structure featuring an ordered vertex list.
+    """ Coplanar polygon data structure featuring an ordered vertex list.
     """
     
     def __init__(self, vertices, order='cw'):
@@ -73,6 +74,7 @@ class Face3D(object):
             raise ValueError('Number of vertices should be more than 3!')
         else:
             # Calculate basic properties of the polygon
+            self.calculate_center()
             self.vertex_order(order=order)
             self.calculate_edges()
             self.calculate_normal()
@@ -81,8 +83,15 @@ class Face3D(object):
         """ Vertex list ordering ('cw' as clockwise or 'ccw' as counterclockwise).
         """
         
+        # For coplanar polygon the point
         keys = list(range(1, self.n_vertex+1))
-        self.vertdict = dict(zip(keys, self.vertices))
+        self.verts_dict = dict(zip(keys, self.vertices))
+
+    def calculate_center(self):
+        """ Calculate the center coordinate of the face polygon.
+        """
+
+        self.center = self.vertices.mean(axis=0)
         
     def calculate_normal(self):
         """ Calculate the unit normal vector of the face in 3D.
@@ -113,5 +122,53 @@ class Face3D(object):
             total += prod
         
         self.area = abs(np.dot(total, self.unit_normal) / 2)
+
+
+class Octahedron(ConvexPolyhedron):
+    """ Octahedral object with coordinate transformation of its vertices and faces.
+    """
+    
+    def __init__(self, n_vertex=6, n_edge=12, poly_type='regular', vertices=None, **kwargs):
+        
+        center = kwargs.pop('center', np.array([0, 0, 0]))
+        super().__init__(self, center=center, n_vertex=n_vertex, n_edge=n_edge)
+        self.poly_type = poly_type
+        self.vertices = vertices
+        
+    def generate_vertices(self, radius, angles=None, alpha=0, beta=0, gamma=0):
+        """ Generate the vertex coordinates of the octahedron.
+        """
+                
+        # Generate the coordinates of vertices
+        if self.poly_type == 'regular': # regular octahedron
+            a = radius
+            self.vertices = np.array([[a, 0, 0], [-a, 0, 0], 
+                                      [0, a, 0], [0, -a, 0],
+                                      [0, 0, a], [0, 0, -a]])
+        elif self.poly_type == 'rhom_bipyramid': # rhombic bipyramid
+            a, b, c = radius
+            self.vertices = np.array([[a, 0, 0], [-a, 0, 0], 
+                                      [0, b, 0], [0, -b, 0],
+                                      [0, 0, c], [0, 0, -c]])
+        elif self.poly_type == 'asym_bipyramid': # asymmetric bipyramid
+            self.vertices = np.array([[radius[0], 0, 0], [radius[1], 0, 0], 
+                                      [0, radius[2], 0], [0, radius[3], 0],
+                                      [0, 0, radius[4]], [0, 0, radius[5]]])
+            
+        # Rotate according to the pose angles
+        if angles is not None:
+            alpha, beta, gamma = angles
+        
+        ctr = self.center
+        transform_list = [trans.translation3D(*ctr), trans.rotation_z(gamma),
+                          trans.rotation_y(beta), trans.rotation_x(alpha),
+                          trans.translation3D(*-ctr)]
+        
+        transform_matrix = np.matmul(*transform_list)
+        self.vertices = np.dot(transform_matrix, self.vertices)
+        
+        # Create vertex list
+        keys = range(1, len(self.n_vertex)+1)
+        self.verts_dict = dict(keys, self.vertices)
 
 
