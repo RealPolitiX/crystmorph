a3adc405ff4d8f52b20f84063dac0f6de404c4e5
RealPolitiX <xrpatrick@gmail.com>
2021-04-19 05:29:37 -0500

    Added JSONExporter and CIFExporter

--- crystmorph/structure.py
+++ crystmorph/structure.py
@@ -8,10 +8,12 @@
 from . import polyhedron as ph, utils as u
 import numpy as np
 import json
+from copy import deepcopy
 from funcy import project
 try:
     import pymatgen
     from pymatgen.core.structure import Structure
+    from pymatgen.core.sites import PeriodicSite
 except:
     pass
 
@@ -25,11 +27,11 @@ class CIFParser(object):
     """ Parser class for crystallographic information files (CIFs).
     """
 
-    def __init__(self, filepath, parser='pymatgen'):
+    def __init__(self, filepath, backend='pymatgen'):
 
-        if parser == 'pymatgen':
+        if backend == 'pymatgen':
             self.struct = Structure.from_file(filepath)
-        elif parser == 'pycifrw':
+        elif backend == 'pycifrw':
             self.struct = ReadCif(filepath)
 
 
@@ -43,6 +45,29 @@ class JSONParser(object):
             self.struct = json.load(f, **kwargs)
 
 
+class CIFExporter(object):
+    """ Exporter class for crystallographic information files (CIFs).
+    """
+
+    def __init__(self, obj, filepath, backend='pymatgen'):
+
+        if backend == 'pymatgen':
+            obj.to(filepath)
+        elif backend == 'pycifrw':
+            with open(filepath) as f:
+                f.write(obj.WriteOut())
+
+
+class JSONExporter(object):
+    """ Exporter class for crystallographic information in json format.
+    """
+
+    def __init__(self, obj, filepath, **kwargs):
+
+        with open(filepath, 'w') as f:
+            json.dump(obj, f, **kwargs)
+
+
 class StructureParser(object):
     """ Parser class for crystal structures. The class funnels the atomistic information
     of a crystal structure into a searchable format using a combination of new methods and
@@ -52,14 +77,17 @@ class StructureParser(object):
     def __init__(self, filepath, form='cif', parser_kwargs={}, **kwargs):
 
         if form == 'cif':
-            parser = CIFParser(filepath, **parser_kwargs)
+            parser = CIFParser(filepath=filepath, **parser_kwargs)
         elif form == 'json':
-            parser = JSONParser(filepath, **parser_kwargs)
+            parser = JSONParser(filepath=filepath, **parser_kwargs)
         self.struct = parser.struct
 
         # Retrieve essential chemical information (default is the atom name, number, Cartesian and fractional coordinates)
         self.atoms_info_list = kwargs.pop('atoms_info_list', ['specie.name', 'specie.number', 'coords', 'frac_coords'])
         self.atoms_dict = u.multiretrieve(self.atoms_info_list, self.struct.sites)
+        
+        self.unique_elements = [s.name for s in self.struct.types_of_specie]
+        self.composition = self.struct.composition.as_dict()
 
     def find_atoms(self, names=None):
         """ Locate the atomic species according to specification.
@@ -70,20 +98,35 @@ class StructureParser(object):
             names = [names]
         for name in names:
             loc = np.where(atom_names == name)
-        selected_atoms_dict = {ai_name: ai_value for ai_name, ai_value in zip(atoms_info_list, atoms_info[loc])}
+        selected_atoms_dict = {ai_name: ai_value[loc] for ai_name, ai_value in self.atoms_dict}
 
         return selected_atoms_dict
 
-    def find_neighbors(self, radius, name=None, idx=None):
+    def find_neighbors(self, radius, name=None, idx=None, include_center_atom=False):
         """ Locate the neighboring atom species according to specification.
         """
 
-        # Retrieve the (crystallographic) site instance
+        # Retrieve the atomic site instance
         site = self.struct.sites[idx]
         neighbors_sites = self.struct.get_neighbors(site, radius)
         neighbors_dict = u.multiretrieve(self.atoms_info_list, neighbors_sites)
+        if include_center_atom:
+            return site.as_dict(), neighbors_dict
+        else:
+            return neighbors_dict
 
-        return neighbors_dict
+    def perturb(self, distance, min_distance=None, keep=False, ret=True):
+        """ Apply random perturbation to the atomic coordinates.
+        """
+
+        struct = deepcopy(self.struct)
+        struct_perturbed = self.struct.perturb(distance, min_distance)
+        if not keep:
+            self.struct = struct
+
+        if ret:
+            return struct_perturbed
 
 
 
+        
